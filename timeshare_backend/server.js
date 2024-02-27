810b7a1346698c72169c604f1052fd264c74a58a
Euan-lc <116933854+Euan-lc@users.noreply.github.com>
2024-02-27 13:08:52 +0700

    vercel fix

--- timeshare_backend/server.js
+++ timeshare_backend/server.js
@@ -6,4 +6,8 @@ const propertyRouter =require('./routes/r_properties');
 const app = express();
 
 app.use('/api/property', propertyRouter);
+app.use('/api/review', reviewRouter);
+
 app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}.`));
+
+module.exports = app;
