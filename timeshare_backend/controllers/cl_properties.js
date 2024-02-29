57febc71fdc3f696fe96eab6852792d5de7a240a
Euan-lc <116933854+Euan-lc@users.noreply.github.com>
2024-02-29 17:10:51 +0700

    backend query hotfix

--- timeshare_backend/controllers/cl_properties.js
+++ timeshare_backend/controllers/cl_properties.js
@@ -1,23 +1,31 @@
-const { pool } = require('../database')
+const {pool} = require('../database')
 
 exports.GetAllProperties = async (req, res) => {
     var limit = parseInt(req.query.limit) ? req.query.limit : 10;
     var offset = parseInt(req.query.offset) ? req.query.offset : 0;
     var sort = req.query.sort_by ? req.query.sort_by.split(':') : ['desc', 'rating'];
+    let end = req.query.availability ? req.query.availability[1].split(':')[1] :  '9999-12-31' ;
+    let start = req.query.availability ? req.query.availability[0].split(':')[1] : '9999-12-31';
 
-    var query = `SELECT p.timeshareId, p.name, p.address, p.price, avg(r.Rating) as rating
-                            from Properties p
-                                left join Reviews r
-                                on p.timeshareId = r.timeshareId
-                        group by p.timeshareId
-                        order by ${sort[1]} ${sort[0]}
-                        LIMIT ${limit}
-                        OFFSET ${offset};`;
+    console.log(req.query)
 
-    console.log(req.query);
+    let query = `SELECT p.timeshareId, p.name, p.address, p.price, rating
+                                        FROM (SELECT p.*, AVG(r.rating) AS rating
+                            FROM Properties p LEFT JOIN Reviews r
+                                ON p.timeshareId = r.timeshareId
+                        GROUP BY p.timeshareId) p LEFT JOIN Contracts c
+                                            ON p.timeshareId = c.timeshareId
+                                        WHERE (NOT (c.startDay <= DATE('${end}') AND c.endDay >= DATE('${start}'))
+                                            OR c.contractId IS NULL)
+                                            ${req.query.price ? 'AND p.price between ' + req.query.price[1].split(':')[1] + ' AND ' + req.query.price[0].split(':')[1] : ''}
+                                        GROUP BY p.timeshareId
+                                        ORDER BY ${sort[1]} ${sort[0]}
+                                        LIMIT ${limit}
+                                        OFFSET ${offset};`;
 
+    console.log(query)
     pool.query(query, (err, result) => {
-        if(err) {
+        if (err) {
             console.error(err);
             return;
         }
@@ -29,7 +37,7 @@ exports.GetAllProperties = async (req, res) => {
 exports.GetSingleProperty = async (req, res) => {
 
     pool.query(`Select * from Properties where Properties.timeshareId = ${req.query.id}`, (err, result) => {
-        if(err) {
+        if (err) {
             console.error(err);
             return;
         }
@@ -40,7 +48,7 @@ exports.GetSingleProperty = async (req, res) => {
 
 exports.GetLocations = async (req, res) => {
     pool.query('SELECT * FROM Properties GROUP BY address', (err, result) => {
-        if(err){
+        if (err) {
             console.error(err);
             return;
         }
@@ -53,7 +61,7 @@ exports.AddNewProperty = async (req, res) => {
     pool.query(`INSERT INTO Properties (name, address, price, capacity, size, description, img) 
                                 VALUES ("${req.query.name}", "${req.query.address}", ${req.query.price}, ${req.query.capacity}, ${req.query.size}, "${req.query.description}", "${req.query.img}")`,
         (err, result) => {
-            if(err){
+            if (err) {
                 console.error(err);
                 return;
             }
