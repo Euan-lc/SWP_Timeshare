9dcd44b7a0f5eac34203eac2a835244c802edfe8
Oleg Krajic <oleg.krajic@epita.fr>
2024-03-13 13:34:57 +0700

    [???] ???

---
+++ timeshare_frontend/src/pages/admin/Admin.jsx
@@ -0,0 +1,10 @@
+import "./admin.css";
+import Sidebar from "../../components/sidebarv2/Sidebar";
+
+export default function Admin() {
+    return (
+        <div>
+            <Sidebar/>
+        </div>
+    )
+}
